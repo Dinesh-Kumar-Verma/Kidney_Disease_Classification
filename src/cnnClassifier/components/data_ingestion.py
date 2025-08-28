52d1fda13c33992e2658eadddad7dc46d72e5abe
Dinesh Kumar Verma <vermadinesh006@gmail.com>
2025-08-28 18:49:41 +0530

    Going to work on full data with the help of google colab

--- src/cnnClassifier/components/data_ingestion.py
+++ src/cnnClassifier/components/data_ingestion.py
@@ -1,38 +1,39 @@
 import os
 import zipfile
-import gdown
+import kaggle
 from cnnClassifier import logger
 from cnnClassifier.utils.common import get_size
 from cnnClassifier.entity.config_entity import (DataIngestionConfig)
-
-
 
 class DataIngestion:
     def __init__(self, config: DataIngestionConfig):
         self.config = config
 
-    
-    def download_file(self)-> str:
+    def download_file(self):
         '''
-        Fetch data from the url
+        Fetch data from Kaggle
         '''
+        try:
+            dataset_name = self.config.source_URL
+            zip_download_dir = os.path.dirname(self.config.local_data_file)
+            os.makedirs(zip_download_dir, exist_ok=True)
+            logger.info(f"Downloading data from Kaggle dataset {dataset_name} into file {self.config.local_data_file}")
 
-        try: 
-            dataset_url = self.config.source_URL
-            zip_download_dir = self.config.local_data_file
-            os.makedirs("artifacts/data_ingestion", exist_ok=True)
-            logger.info(f"Downloading data from {dataset_url} into file {zip_download_dir}")
+            # Authenticate with Kaggle API
+            kaggle.api.authenticate()
 
-            file_id = dataset_url.split("/")[-2]
-            prefix = 'https://drive.google.com/uc?/export=download&id='
-            gdown.download(prefix+file_id,zip_download_dir)
+            # Download the dataset
+            kaggle.api.dataset_download_files(dataset_name, path=zip_download_dir, unzip=False)
+            
+            # Rename the downloaded file
+            downloaded_file_name = dataset_name.split('/')[1] + '.zip'
+            os.rename(os.path.join(zip_download_dir, downloaded_file_name), self.config.local_data_file)
 
-            logger.info(f"Downloaded data from {dataset_url} into file {zip_download_dir}")
+            logger.info(f"Downloaded data from {dataset_name} into file {self.config.local_data_file}")
 
         except Exception as e:
+            logger.exception(e)
             raise e
-        
-    
 
     def extract_zip_file(self):
         """
@@ -44,4 +45,3 @@ class DataIngestion:
         os.makedirs(unzip_path, exist_ok=True)
         with zipfile.ZipFile(self.config.local_data_file, 'r') as zip_ref:
             zip_ref.extractall(unzip_path)
-
